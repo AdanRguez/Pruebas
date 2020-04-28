bc612981cf028e9e7816bb38d75946ca08570957
AdanRguez <adan_090@hotmail.com>
2020-04-28 17:43:23 -0600

    primer commit

---
+++ MovilPruebas/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MovilPruebas.Models;
+
+namespace MovilPruebas.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
